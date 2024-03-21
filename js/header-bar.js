3142fddf761851346eb7949abe3456acba1150b4
ha7darov <vi.yeoun.vi@gmail.com>
2024-03-21 22:17:44 +0500

    new

--- js/header-bar.js
+++ js/header-bar.js
@@ -1,8 +1,9 @@
-function hide(elementId) {
-    let list = document.getElementById(elementId);
-    if (list.style.display === "none") {
-        list.style.display = "block";
-    } else {
-        list.style.display = "none";
-    }
-}
+
+        function hide(elementId) {
+            let list = document.getElementById(elementId);
+            if (list.style.display === "none") {
+                list.style.display = "block";
+            } else {
+                list.style.display = "none";
+            }
+        }
